0de0e34278801093b0808fe262103179ee350a5f
bubux29 <freebubux@gmail.com>
2018-06-14 22:26:11 +0200

    On laisse béton l'encode UTF-8 à la mano
    This reverts commit 885efb4b395f6c482d3cfdc5b9211dce4e26e251.

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from SnsmMain import SnsmMain
 import formation_db
 
 if __name__ == "__main__":
-	info('DB', u'Connection \u00e0 la database')
+	info('DB', 'Connection à la database')
 	formation_db.init_db()
 	formation_db.connect_to_db()
 	SnsmMain().run()
-	info('DB', u'D\u00e9connection de la database')
+	info('DB', 'Déconnection de la database')
 	formation_db.disconnect_db()
